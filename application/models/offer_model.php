de3259f89f4888656f7342d03e81a270f786304a
Brajola <brajola@gmail.com>
2011-08-22 21:48:03 -0300

    Model de ofertas de produtos

---
+++ application/models/offer_model.php
@@ -0,0 +1,31 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Offer_model extends CI_model{
+	
+	private $tablename;
+	
+	function __construct()
+	{
+		parent::__construct();
+		$this->tablename = 'offer';
+	}
+	
+	function all()
+	{
+		$this->db->select('*');
+		$this->db->from($this->tablename);
+		$this->db->where('status_id', 1);
+		$this->db->order_by('name', 'ASC');
+		
+		$query = $this->db->get();
+		$data  = array();
+		
+		if($query->num_rows() > 0){
+			foreach ($query->result() as $row){
+				array_push($data, $row);
+			}
+		}
+		
+		return $data;
+	}
+}
