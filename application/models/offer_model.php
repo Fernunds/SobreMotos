53661b63031b7e12b2e5931a8a9547c811f6ef7f
Brajola <brajola@gmail.com>
2011-08-22 22:44:57 -0300

    Refactoring do model de offers para ficar mais elegante.

--- application/models/offer_model.php
+++ application/models/offer_model.php
@@ -3,11 +3,41 @@
 class Offer_model extends CI_model{
 	
 	private $tablename;
+	private $queryfields;
+	private $queryjoins;
 	
 	function __construct()
 	{
 		parent::__construct();
 		$this->tablename = 'offer';
+		$this->queryfields = '
+			offer.id,
+			brand.name as brand,
+			model.name as model,
+			fuel.name as fuel,
+			exchange.name as exchange,
+			color.name as color,
+			state.name as state,
+			city.name as city,
+			offer.title,
+			offer.slug,
+			offer.description,
+			offer.factory_year,
+			offer.model_year,
+			offer.mileage,
+			offer.plate,
+			offer.price,
+			offer.is_funding';
+			
+		$this->queryjoins = array(
+			array('brand', 'brand.id=offer.brand_id'),
+			array('model', 'model.id=offer.model_id'),
+			array('fuel', 'fuel.id=offer.fuel_id'),
+			array('exchange', 'exchange.id=offer.exchange_id'),
+			array('color', 'color.id=offer.color_id'),
+			array('state', 'state.id=offer.state_id'),
+			array('city', 'city.id=offer.city_id')
+		);
 	}
 	
 	function all()
@@ -31,39 +61,40 @@ class Offer_model extends CI_model{
 	
 	function featured($amount = 4)
 	{
-		$this->db->select('
-			offer.id,
-			brand.name as brand,
-			model.name as model,
-			fuel.name as fuel,
-			exchange.name as exchange,
-			color.name as color,
-			state.name as state,
-			city.name as city,
-			offer.title,
-			offer.slug,
-			offer.description,
-			offer.factory_year,
-			offer.model_year,
-			offer.mileage,
-			offer.plate,
-			offer.price,
-			offer.is_funding
-		');
-		
+		$this->db->select($this->queryfields);
 		$this->db->from($this->tablename);
 		
-		$this->db->join('brand', 'brand.id=offer.brand_id');
-		$this->db->join('model', 'model.id=offer.model_id');
-		$this->db->join('fuel', 'fuel.id=offer.fuel_id');
-		$this->db->join('exchange', 'exchange.id=offer.exchange_id');
-		$this->db->join('color', 'color.id=offer.color_id');
-		$this->db->join('state', 'state.id=offer.state_id');
-		$this->db->join('city', 'city.id=offer.city_id');
+		for($i=0; $i<count($this->queryjoins); $i++){
+			$this->db->join($this->queryjoins[$i][0], $this->queryjoins[$i][1]);
+		}
 		
 		$this->db->where('offer.status_id', 1);
 		$this->db->order_by('offer.title', 'ASC');
 		$this->db->limit($amount);
+		
+		$query = $this->db->get();
+		$data  = array();
+		
+		if($query->num_rows() > 0){
+			foreach ($query->result() as $row){
+				array_push($data, $row);
+			}
+		}
+		
+		return $data;
+	}
+	
+	function details($slug)
+	{
+		$this->db->select($this->queryfields);
+		$this->db->from($this->tablename);
+		
+		for($i=0; $i<count($this->queryjoins); $i++){
+			$this->db->join($this->queryjoins[$i][0], $this->queryjoins[$i][1]);
+		}
+		
+		$this->db->where('offer.status_id', 1);
+		$this->db->where('offer.slug', $slug);
 		
 		$query = $this->db->get();
 		$data  = array();
