8063ac47e64945d35cb97e23e5ea0a07940026ed
Fernunds <fernunds@gmail.com>
2011-09-16 16:05:55 -0300

    Ajuste de Layout

--- application/views/common/footer.php
+++ application/views/common/footer.php
@@ -7,13 +7,18 @@
         
 		  <div id="linkMap">
           	<div id="boxLinkMap">
-        	  <!-- links do rodapé -->
-              <!--<p><strong>Classificados</strong></p>-->
+        	  <!-- links do rodapé --
+              <p><strong>Serviços</strong></p>
+              <ul>
+              	<li>Comprar</li>
+                <li>Vender</li>
+              </ul> --><br />
+              <p>Sites do SobreMotos: <a href="http://www.sobremotos.com.br" target="_blank" title="Revista">Revista</a> | <a href="/" title="Classificados">Classificados</a></p>
             </div>
           </div>
           <div id="copyright">
             <div id="boxCopyright">
-			<p>Todo o conteúdo deste site é de uso exclusivo. Proibida reprodução ou utilização a qualquer título, sob as penas da lei.</p>
+			<p>Todo o conteúdo deste site é de uso exclusivo. Proibida reprodução ou utilização a qualquer título, sob as penas da lei. <a href="#" title="Política de Privacidade">Política de Privacidade</a></p>
             </div>
 		</div>
 	</body>
