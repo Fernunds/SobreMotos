432d6c6ae71bfc79b8168aadd473faa498fac163
Fernunds <fernunds@gmail.com>
2011-09-23 14:03:19 -0300

    Ajustes de CSS da Home
    Inseri também o logo da FIPE

--- application/views/common/search.php
+++ application/views/common/search.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <div id="col1">	
 	<h3>Compre sua Moto</h3>
 	<form method="POST" action="<?=$root?>buscar">
@@ -60,6 +61,67 @@
 					<select name="min_year" id="min_year">
 						<option value="" selected="selected">de</option>
 						<?php
+=======
+<div id="col1">
+  <h3>Compre sua Moto</h3>
+  <form method="get" action="<?=$root?>buscar">
+    <fieldset>
+      <legend>Formulário de Busca</legend>
+      <ol>
+        <li>
+          <label for="brand_id">Marca:</label>
+          <select name="brand_id" id="brand_id" class="formHome">
+          </select>
+        </li>
+        <li>
+          <label for="model_id">Modelo:</label>
+          <select name="model_id" id="model_id" class="formHome" style="width:170px;">
+          </select>
+        </li>
+        <li>
+          <label for="min_price">Preço:</label>
+          <select name="min_price" id="min_price">
+            <option value="" selected>de</option>
+            <option value="5000">5.000</option>
+            <option value="10000">10.000</option>
+            <option value="15000">15.000</option>
+            <option value="20000">20.000</option>
+            <option value="25000">25.000</option>
+            <option value="30000">30.000</option>
+            <option value="35000">35.000</option>
+            <option value="40000">40.000</option>
+            <option value="50000">50.000</option>
+            <option value="60000">60.000</option>
+            <option value="70000">70.000</option>
+            <option value="80000">80.000</option>
+            <option value="90000">90.000</option>
+            <option value="99999">100.000</option>
+          </select>
+          <select name="max_price" id="max_price">
+            <option value="" selected="selected">até</option>
+            <option value="5000">5.000</option>
+            <option value="10000">10.000</option>
+            <option value="15000">15.000</option>
+            <option value="20000">20.000</option>
+            <option value="25000">25.000</option>
+            <option value="30000">30.000</option>
+            <option value="35000">35.000</option>
+            <option value="40000">40.000</option>
+            <option value="50000">50.000</option>
+            <option value="60000">60.000</option>
+            <option value="70000">70.000</option>
+            <option value="80000">80.000</option>
+            <option value="90000">90.000</option>
+            <option value="99999">100.000</option>
+            <option value="999000">999.000</option>
+          </select>
+        </li>
+        <li>
+          <label for="min_year">Ano:</label>
+          <select name="min_year" id="min_year">
+            <option value="" selected="selected">de</option>
+            <?php
+>>>>>>> Ajustes de CSS da Home
 						for($i=date('Y')+1; $i>1960; $i--){
 							print('<option value="'.$i.'">'.$i.'</option>');
 						}
@@ -93,15 +155,18 @@
   <hr />
   <br />
   <h3>Anuncie sua Moto</h3>
-  <p><a href="" title="Anunciar" class="button buttonConfirm">Anunciar</a></p><br />
-  <a href="" title="">Loja ou Concessionária</a><br />
-  <a href="" title="">Quanto custa anunciar?</a>
+  <p><a href="" title="Anunciar" class="button buttonConfirm">Anunciar</a></p>
+  <br />
+  <p><a href="" title="">Loja ou Concessionária</a></p>
+  <br />
   <hr />
   <p><br />
     <span class="txtPublicidade">Publicidade<br />
     </span> <img src="resources/images/banner/gm250x250.jpg" alt="" width="250" height="250" /></p>
+  <br />
+  <br />
   <h3>Avalie seu veículo</h3>
-  <a href="" title="">Tabela Fipe</a><br />
-  <a href="" title="">Tabela Molicar</a>
-  <hr />  
+  <a href="http://fipe.org.br/web/indices/veiculos/default.aspx?v=m&p=52" target="_blank" title="Tabela Fipe - Avalie sua moto"><img src="resources/images/fipe.gif" /></a>
+  <p><a href="http://fipe.org.br/web/indices/veiculos/default.aspx?v=m&p=52" target="_blank" title="Tabela Fipe - Avalie sua moto">Tabela Fipe</a></p>
+  <hr />
 </div>
